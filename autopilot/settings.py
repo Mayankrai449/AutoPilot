b31301f2b3865d05b0f96933beafbd979b080ad5
Mayank Rai <mayankraivns@gmail.com>
2024-08-05 19:32:12 +0530

    secretkey update

--- autopilot/settings.py
+++ autopilot/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure---y^(s7-#bax(!oj1qf-e%#r!jg6fmvya6k2$(7n!#1q!d)j4t"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
